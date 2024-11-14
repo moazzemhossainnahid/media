25df1be218ad16cdd11eae49cc6fac06063563b3
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2024-11-14 19:02:05 +0600

    resolve clerk

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,18 +1,33 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in', '/sign-up'])
+// const isPublicRoute = createRouteMatcher(['/sign-in', '/sign-up'])
 
-export default clerkMiddleware(async (auth, request) => {
-    if (!isPublicRoute(request)) {
-        await auth.protect()
-    }
-})
+// export default clerkMiddleware(async (auth, request) => {
+//     if (!isPublicRoute(request)) {
+//         await auth.protect()
+//     }
+// })
+
+// export const config = {
+//     matcher: [
+//         // Skip Next.js internals and all static files, unless found in search params
+//         '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//         // Always run for API routes
+//         '/(api|trpc)(.*)',
+//     ],
+// }
+
+
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher(["/", "/documents", "/media", "/images", "/others"]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) {
+    auth.protect();
+  }
+});
 
 export const config = {
-    matcher: [
-        // Skip Next.js internals and all static files, unless found in search params
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        // Always run for API routes
-        '/(api|trpc)(.*)',
-    ],
-}
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
